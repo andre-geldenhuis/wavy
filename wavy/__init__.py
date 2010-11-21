818b4b936172356923c1443e5ea6ec613009079c
Nicolas Louveton <nblouveton@gmail.com>
2010-11-21 15:32:15 +0100

    adding psyco import

---
+++ wavy/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2010, Nicolas Louveton <nblouveton@gmail.com>
+#
+#    This file is part of Wavy.
+#
+#    Wavy is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Wavy is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Wavy.  If not, see <http://www.gnu.org/licenses/>.
+
+_VERSION = '0.9'
+
+try:
+    import psycho
+    psycho.full()
+except ImportError:
+    print('No Just in Time (psyco) compiler found.')
