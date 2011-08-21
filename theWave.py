e7874ec5914854866c3796c8e89e9a399bc2a2b3
Nicolas Louveton <nblouveton@gmail.com>
2011-08-21 17:53:57 +0200

    Better documentation

--- theWave.py
+++ theWave.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+theWave.py script use wavy as a digitial video input
+to auditory stream sensory substitution system.
+It provide a similar system to theVibe.
+'''
+
 from wavy.thewavelib import TheWaveMachine
 
 # Launching theWave
